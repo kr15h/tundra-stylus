09a4bb9303a3f790cc1a5b20d9a8b368570e69fb
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2026-02-16 16:47:16 +0200

    Make sure we can open Projected View after closing again

--- software/webui/js/widgets/viewopts.js
+++ software/webui/js/widgets/viewopts.js
@@ -1,26 +1,36 @@
 export class ViewOptions {
 	constructor() {
 		this.viewOptionsElement = document.getElementById('view_options');
-		this.window = null;
+		this.pvWindow = null; // pv - Projected View
+		this.pvCloseCheckInterval = null;
 
 		const buttons = this.viewOptionsElement.getElementsByTagName('button');
 		for (const button of buttons) {
 			if (button.textContent == 'Projected View') {
 				button.addEventListener('click', (e) => {
-					this.openInWindow();
+					this.openProjectedView();
 				});
 			}		
 		}
 	}
 
-	openInWindow() {
-		if (!this.window) {
-			this.window = window.open(
+	openProjectedView() {
+		if (!this.pvWindow) {
+			this.pvWindow = window.open(
 				'views/projected/index.html',
 				'Projected View',
 				'width=600,height=400'
 			);
+
+			this.pvCloseCheckInterval = setInterval(() => {
+  				if (!this.pvWindow || this.pvWindow.closed) {
+    				clearInterval(this.pvCloseCheckInterval);
+    				console.log('Projected View window closed');
+					this.pvWindow = null;
+  				}
+			}, 500);
 		}
 	}
+
 }
 
